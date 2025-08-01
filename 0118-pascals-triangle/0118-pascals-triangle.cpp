bf88c81a2eb1bb2ca0f1153716ea73ba58b89cad
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-01 19:45:05 +0530

    Time: 0 ms (100%), Space: 9.7 MB (30.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>triangle;
-        if(numRows==1) return {{1}};
-        if(numRows==2) return {{1},{1,1}};
-        triangle.push_back({1});
-        triangle.push_back({1,1});
-        for(int i=2;i<numRows;i++){
-            vector<int>v(i+1);
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i) v[j]=1;
-                else v[j]=triangle[i-1][j]+triangle[i-1][j-1];
-            }
-            triangle.push_back(v);
+        vector<vector<int>>ans ;
 
+        for(int i = 0 ; i < numRows ; i ++){
+            vector<int>nthrow(i + 1 , 0) ;
+            nthrow[0] = 1 , nthrow[i] = 1 ;
+
+            for (int j = 1 ; j < i ; j++){
+                nthrow[j] = ans[i - 1][j - 1] + ans[i - 1][j] ;
+            }
+
+            ans.push_back(nthrow) ;
         }
-        return triangle;
+
+
+        return ans ;
+        
     }
 };

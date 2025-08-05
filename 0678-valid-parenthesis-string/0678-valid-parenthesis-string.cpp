73d8d7f97fe01dea3710b3d071906f606dec24e9
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-06 02:42:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.53%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.size() ;
+        int mini = 0 , maxi = 0  ;
+
+        for (int i = 0 ; i < n ; i ++){
+            if(s[i] == '(') mini = mini+ 1 , maxi = maxi + 1 ;
+            else if(s[i] == ')') mini = mini - 1 , maxi = maxi - 1 ;
+            else mini = mini - 1 , maxi = maxi + 1 ;
+        
+        if(mini < 0) mini = 0 ;
+
+        if (maxi < 0) return false ;
+    }
+        return mini == 0 ;
+    }
+};

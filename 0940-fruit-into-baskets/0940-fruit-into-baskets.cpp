3e8dac262c871abd7d27522ecfa0e7d70072df7e
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-06 00:34:53 +0530

    Time: 23 ms (91.46%), Space: 75.1 MB (57.12%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -4,21 +4,48 @@ public:
 
         int n = fruits.size() ;
 
+        int l = 0 , r = 0 ;
 
-        unordered_map <int , int > mpp ;
+        int maxLen = 0 ;
 
-        for (auto &i : fruits){
-            mpp[i] ++;
+        unordered_map<int , int > mpp ; 
+
+
+        while (r < n){
+            mpp[fruits[r]] ++;
+
+            if(mpp.size() <= 2) maxLen = max (maxLen  , r - l + 1) ; 
+
+            else {
+                mpp[fruits[l]] -- ;
+                if(mpp[fruits[l]] == 0)
+                mpp.erase(fruits[l]) ;
+                l ++ ;
+                maxLen = max(maxLen , r - l + 1) ;
+            }
+
+            
+         r++ ;
+            
         }
 
-        int max1 = 0 , max2 = 0 ;
-        for (auto &i : mpp){
-            if(i.second >= max1) max2 = max1  , max1 = i.second ;
-            else if (i.second < max1 && i.second >= max2) max2 = i.second ;
-        }
 
-        return max1 + max2 ;
-        
+        // unordered_map <int , int > mpp ;
+
+        // for (auto &i : fruits){
+        //     mpp[i] ++;
+        // }
+
+        // int max1 = 0 , max2 = 0 ;
+        // for (auto &i : mpp){
+        //     if(i.second >= max1) max2 = max1  , max1 = i.second ;
+        //     else if (i.second < max1 && i.second >= max2) max2 = i.second ;
+        // }
+
+        // return max1 + max2 ;
+
+
+        return maxLen ;
         
     }
 };

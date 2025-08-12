220d9ee75c77d740be07ba10ff228aa1cc64f828
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-13 04:48:25 +0530

    Time: 3044 ms (5.01%), Space: 385.2 MB (7.19%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int modi ; 
+
+    int solve(int n , int x , int idx , int ans , vector<vector<int>>&dp) {
+        modi = 1e9 + 7 ;
+        if (ans > n) return 0 ;
+        if (ans == n) return 1 ;
+
+        if (dp[idx][ans] != -1) return dp[idx][ans] ;
+
+        int res = 0 ;
+
+        for (int i = idx ; i <= n ; i++){
+            long long val = pow(i , x) ;
+            if (ans + val > n) break ;
+            res = (res + solve (n , x , i + 1 , ans + val , dp)) % modi ;       
+            
+        }
+
+        return dp[idx][ans] = res % modi  ;
+
+    }
+
+    int numberOfWays(int n, int x) {
+        modi = 1e9 + 7 ;
+        int ans = 0 ;
+        vector<vector<int>>dp (301 , vector<int>(301 , -1)) ;
+        ans = solve (n , x , 1 , 0 , dp) ;
+        return ans % modi ;
+    }
+};

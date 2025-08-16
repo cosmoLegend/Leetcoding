4ef76585d9a6f78257c68ea6725064e59fe53d12
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-16 17:53:01 +0530

    Time: 0 ms (100%), Space: 12.2 MB (44.39%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    void dfs (int i , int j , vector<vector<char>> & board , int n , int m){
-        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != 'X' ) return ;
-
-        board[i][j] = '.' ;
-
-        dfs (i + 1 ,  j , board , n , m) ;
-        dfs (i , j + 1 , board , n , m) ;
-        dfs (i , j - 1 , board , n , m) ;
-        dfs (i - 1 , j , board , n , m) ;
-    }
-
     int countBattleships(vector<vector<char>>& board) {
-        int ans = 0 ;
-        int n = board.size() ;
-        int m = board[0].size() ;
-
-
-        for (int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j < m ; j ++){
-                if (board[i][j] == 'X'){
-                    ans ++ , dfs (i , j , board , n , m) ;
-                }
+        int count = 0 ;
+        for (int i = 0 ; i < board.size() ; i++){
+            for(int j = 0 ; j < board[0].size() ; j++){
+                if(board[i][j] == 'X' && (i == 0 || board[i - 1][j]!= 'X') && (j == 0 || board[i][j - 1] != 'X')) count ++ ;
             }
         }
 
 
-        return ans ;
+        return count ;
+        
     }
 };

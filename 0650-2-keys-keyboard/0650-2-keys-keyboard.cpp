b8e9ff16a480c2ddf09f9225f526781dbbf04e19
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-24 03:50:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (78.38%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve (int n , int curr , int paster, vector<vector<int>>& dp){
-        if (n == curr) return 0 ;
-        if (curr > n) return 1e8 ;
-
-        if (dp[curr][paster] != -1) return dp[curr][paster] ;
-
-        
-        int copyPaste = 2 + solve (n, curr * 2 , curr , dp) ;
-        int paste = 1 + solve (n , curr + paster , paster , dp) ;
-
-        return dp[curr][paster] = min(copyPaste , paste) ;
-    }
-
     int minSteps(int n) {
-        if (n == 1) return 0  ;
-        vector<vector<int>>dp(n + 1 , vector<int> (n + 1 , -1));
 
-        int ans = solve (n , 1 , 1 , dp) ;
+        int paste = 1 ;
+        int copy = 0 ;
+        int total = 0 ;
 
-        return ans + 1;
+        while (paste != n){
+            if (n % paste == 0){
+                copy = paste ; 
+                total ++ ;
+            }
+
+            paste += copy ;
+            total ++ ;
+
+        }
+
+
+
+        return total ;
         
     }
 };

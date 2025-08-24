37fcd5bb6899d3c8a6e62387a09cdb5b69ea5e7e
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-24 21:44:05 +0530

    Time: 83 ms (12.5%), Space: 280.1 MB (12.5%) - LeetHub

---
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        unordered_map <int , int> mpp ; 
+        int n = nums.size() ;
+
+        if (n % k != 0) return false ;
+
+        for (int i = 0 ; i < n ; i++){
+            mpp[nums[i]] ++ ;
+        }
+
+        vector<pair<int,int>>freqMap ;
+
+        for (auto &i : mpp) freqMap.push_back({i.second ,i.first}) ;
+
+        sort (freqMap.rbegin() , freqMap.rend()) ;
+
+        int groups = n / k ;
+
+        for (int i = 0 ; i < freqMap.size() ; i--){
+            if (freqMap[i].first > groups) return false ;
+            else n -= freqMap[i].first ;
+        }
+
+        return true ;
+
+    }
+};

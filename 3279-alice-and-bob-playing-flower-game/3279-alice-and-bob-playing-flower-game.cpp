c245faef80cc2870ee2c92b6d1b87be169d70d83
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-08-30 14:43:20 +0530

    Time: 0 ms (100%), Space: 8.5 MB (13.62%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        if (m > n) swap(n , m) ;
+        
         long long ans = 0 ;
 
         ans = 1ll * n / 2 * ((m + 1)/ 2) + 1ll *  m / 2 * ((n + 1) / 2) ;

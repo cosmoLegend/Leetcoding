29d4546a9d2b66602de5b29396946fc79eb3323f
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-02 19:24:02 +0530

    Time: 4 ms (15.86%), Space: 22 MB (72.6%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st ;
+        int n = asteroids.size() ;
+        
+        for (int i = 0 ; i < n ; i++){
+            bool flag = true ;
+            while (!st.empty() && st.top() > 0 && asteroids[i] < 0){
+                if (abs(asteroids[i]) > abs(st.top())){
+                    st.pop() ;
+                    
+                }
+                else if (abs(asteroids[i]) == abs(st.top())){
+                    st.pop() ;
+                    flag = false ;
+                    break ;
+                }
+                
+                else {
+
+                    flag = false ;   
+                    break ;
+                    
+                }          
+               
+            }
+
+            if (flag) st.push(asteroids[i]) ;
+        }
+
+        vector<int> result ;
+        while (!st.empty()){
+            int top = st.top() ;
+            st.pop() ;
+            result .push_back (top) ;
+        }
+
+        reverse(result.begin() , result.end()) ;
+        return result ;
+    }
+};

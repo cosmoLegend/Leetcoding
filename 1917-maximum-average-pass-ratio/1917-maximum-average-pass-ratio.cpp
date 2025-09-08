47b3db44e4d04979874fa50b95c6dbcd418833b6
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-09 04:01:15 +0530

    Time: 289 ms (64.38%), Space: 97.9 MB (60.12%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        int n = classes.size() ;
+        priority_queue<pair<double , int>>pq ;
+
+        for (int i = 0 ; i < n ; i++){
+            pq.push({(double)(classes[i][0] + 1) / (classes[i][1] + 1) - (double)classes[i][0]/ classes[i][1] , i}) ;
+        }
+
+        for (int i = 0 ; i < extraStudents ; i++){
+            double gain = pq.top().first ;
+            int idx = pq.top().second ;
+            pq.pop() ;
+            classes[idx][0] ++ ;
+            classes[idx][1] ++ ;
+            pq.push({(double)(classes[idx][0] + 1) / (classes[idx][1] + 1) - (double)classes[idx][0]/ classes[idx][1], idx}) ;
+
+            
+        }
+
+        double sum = 0 ;
+
+        for (auto &c : classes){
+            sum += (double) c[0] / c[1] ;
+        }
+
+
+        return sum / n ; 
+
+    }
+};

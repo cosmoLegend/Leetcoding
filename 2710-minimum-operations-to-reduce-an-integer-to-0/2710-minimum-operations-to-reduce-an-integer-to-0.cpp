d7f2fc8e50dcbf40e1543b7a9de195ac25f57765
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-09 06:17:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.84%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int minOperations(int n) {
-        long long x = n;   // use wider type to avoid overflow when we do x++
-        int ans = 0;
-        while (x > 0) {
-            if ((x & 3) == 3) {    // trailing bits ..11 → better to add 1
-                x++;
-                ans++;
-            } else if (x & 1) {    // trailing bit 1 → subtract 1
-                x--;
-                ans++;
-            } else {               // trailing bit 0 → shift (no counted op)
-                x >>= 1;
-            }
+        int res = 0 ;
+
+        for (int i = 0 ; i < 17 ; i ++){
+            if (__builtin_popcount(n + (1 << i)) < __builtin_popcount(n))
+            res ++ , n += 1 << i ;
         }
-        return ans;
+
+        return res + __builtin_popcount(n) ;
     }
 };

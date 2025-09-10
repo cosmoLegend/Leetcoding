6501fd9784c11c042d7132aec8b1a6239d922e5b
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-10 19:08:56 +0530

    Time: 246 ms (36.47%), Space: 153.2 MB (12.94%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -21,19 +21,21 @@ public:
         
         // step 2: for each language, count coverage
         vector<int> cnt(n+1, 0);
+        int ans = 0 ;
         for (int person : badPeople) {
             for (int lang : languages[person]) {
                 cnt[lang]++;
-                
+                ans = max (ans , cnt[lang]) ;
             }
         }
+
         
-        // step 3: minimize teachings
-        int total = badPeople.size();
-        int ans = INT_MAX;
-        for (int lang = 1; lang <= n; lang++) {
-            ans = min(ans, total - cnt[lang]);
-        }
-        return ans;
+        // // step 3: minimize teachings
+         int total = badPeople.size();
+        // int ans = INT_MAX;
+        // for (int lang = 1; lang <= n; lang++) {
+        //     ans = min(ans, total - cnt[lang]);
+        // }
+        return total - ans;
     }
 };

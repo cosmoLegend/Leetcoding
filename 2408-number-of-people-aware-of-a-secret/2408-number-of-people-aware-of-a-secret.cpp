06849d079e5df2f3b7935cf82baccb0c23f0ef33
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-10 17:29:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
+    int solve (int num , int n , int delay , int forget , vector<int> &dp){
+        if (num == 1) return 1 ;
+        if (dp[num] != -1) return dp[num] ;
+        int result = 0 ; 
+
+        for (int i = num - forget + 1 ; i <= num - delay ; i++){
+            if (i > 0){
+                result += solve(i , n , delay , forget , dp) ;
+            }
+        }
+
+
+        return dp[num] = result ;
+    }
+
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        const int MOD = 1e9 + 7;
-        queue<pair<int, long long>> q; 
-        q.push({1, 1}); // {day_learned, count}
-        
-        int day = 1;
-        while (!q.empty() && day <= n) {
-            int size = q.size();
-            long long cnt = 0;
-            
-            while (size--) {
-                auto [learnDay, people] = q.front();
-                q.pop();
-                
-                if (learnDay + forget > day) { // still remember
-                    if (learnDay + delay <= day) {
-                        cnt = (cnt + people) % MOD;
-                    }
-                    q.push({learnDay, people}); // keep them in queue
-                }
-            }
-            
-            if (cnt > 0) {
-                q.push({day, cnt}); // new learners on this day
-            }
-            
-            day++;
+        int ans = 0 ;
+
+        vector<int>dp (n + 1 , -1) ;
+        for (int i = n - forget + 1 ; i <= n  ; i++){
+            ans += solve (i , n , delay , forget , dp) ;
         }
-        
-        long long ans = 0;
-        while (!q.empty()) {
-            ans = (ans + q.front().second) % MOD;
-            q.pop();
-        }
-        
-        return (int)ans;
+
+        return ans ;
     }
 };

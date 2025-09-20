827fb0f9c9c56cba7b0d98196ba6c54f59f00e0f
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-20 13:49:45 +0530

    Time: 3 ms (20.42%), Space: 9.5 MB (62.69%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        int n = s.size() ;
+        stack<char>st ;
+        vector<int>vis(26 , 0) , cnt (26 , 0) ;
+
+        for (int i = 0 ; i < n ; i++){
+            cnt[s[i] - 'a'] ++ ;
+        }
+
+        for (int i = 0 ; i < n ; i++){
+            cnt [s[i] - 'a'] -- ;
+
+            if (!vis[s[i] - 'a']){
+                while (!st.empty() && cnt[st.top() - 'a'] > 0 && s[i] < st.top()){
+                    vis[st.top() - 'a'] = 0 ; 
+                    st.pop() ;
+                }
+
+                st.push(s[i]) ;
+                vis[s[i] - 'a'] = 1 ;
+            }
+        }
+
+        string result = "" ;
+
+        while (!st.empty()){
+            result += st.top() ;
+            st.pop() ;
+        }
+
+        reverse(result.begin() , result.end()) ;
+
+        return result ;
+        
+    }
+};

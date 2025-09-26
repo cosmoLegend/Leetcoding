ff1fdab846d1b077e2fef8ae23f45045f9281b36
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-27 04:52:39 +0530

    Time: 762 ms (9.84%), Space: 16.6 MB (74.73%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -8,16 +8,16 @@ public:
         sort(nums.begin(), nums.end());
         int cnt = 0;
 
+
         for (int i = 0; i < n - 2; ++i) {
             for (int j = i + 1; j < n - 1; ++j) {
-                int num = nums[i] + nums[j];
-                auto it = lower_bound(nums.begin() + (j + 1), nums.end(), num);
-                cnt += it - (nums.begin() + (j + 1));
+                int k = j + 1 ; 
+                while (k < n && nums[i] + nums[j] > nums[k]) k++ ;
+                cnt += k - j - 1 ;
             }
         }
 
-        if (cnt < 0)
-            return 0;
+        
         return cnt;
     }
 };

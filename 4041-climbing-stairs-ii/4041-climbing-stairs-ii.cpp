56388c59fa6f8a6a01fb2134d9b07297f007389e
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-28 04:23:34 +0530

    Time: 12 ms (50%), Space: 184.9 MB (12.5%) - LeetHub

--- 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
@@ -6,22 +6,19 @@ public:
         vector<int> dp(m + 1, INT_MAX);
         dp[0] = 0;
 
-       
         vector<int> tmp(m + 1, 0);
         for (int i = 1; i <= m; ++i)
             tmp[i] = costs[i - 1];
 
-        for (int i = 0; i <= m; i++) { 
-            if (dp[i] == INT_MAX)
-                continue;
-            int lim = min(m, i + 3); 
-            for (int j = i + 1; j <= lim;
-                 j++) { 
+        for (int i = 0; i <= m; i++) {
+            
+            int lim = min(m, i + 3);
+            for (int j = i + 1; j <= lim; j++) {
                 int d = j - i;
-                dp[j] = min(dp[j], dp[i] + tmp[j] + d*d) ;
+                dp[j] = min(dp[j], dp[i] + tmp[j] + d * d);
             }
         }
 
-        return dp[m]; 
+        return dp[m];
     }
 };

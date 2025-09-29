d55483808342ce83037c1ac5590ce58d828855d3
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-30 02:10:24 +0530

    Time: 5 ms (70.13%), Space: 13.7 MB (37.22%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        map<int , int> mpp ;
+        int n = s.size() ;
+        int l = 0 ;
+        int maxLen = 0 , len = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(mpp.find(s[i]) == mpp.end()) {
+                mpp[s[i]] = 1 ;
+                // len ++ ;
+                maxLen = max(maxLen , i - l + 1) ;
+            }
+            else {
+                    while(s[l] != s[i]) mpp.erase(s[l]) , l++ ;
+                    mpp[s[l]] = 1 ;
+                    l++ ;
+                    
+                }
+                // mpp[s[i]] = 1 ;
+            }
+        
+
+
+        return max(maxLen , len) ;
+        
+    }
+};

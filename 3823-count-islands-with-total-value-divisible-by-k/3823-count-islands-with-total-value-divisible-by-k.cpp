3f8c2ff04b5e97ebd052a56ba44fc62ad51dbc4d
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-09-30 02:47:43 +0530

    Time: 27 ms (79.65%), Space: 84.4 MB (74.93%) - LeetHub

--- 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.cpp
+++ 3823-count-islands-with-total-value-divisible-by-k/3823-count-islands-with-total-value-divisible-by-k.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    int dr[4] = {-1 , 0 , 1 , 0} ;
-    int dc[4] = {0 , -1 , 0 , 1} ;
-    int n , m ;
+    int dr[4] = {-1, 0, 1, 0};  // up, left, down, right
+    int dc[4] = {0, -1, 0, 1};
+    int n, m;
 
-    int sumdfs(vector<vector<int>>&grid , int i , int j){
-        if(i < 0 || i >= n || j < 0 || j >= m ||  grid[i][j] == 0) return 0 ;
-        long sum = 0 ;
-        int tmp = grid[i][j] ;
-        grid[i][j] = 0 ;
-        sum += sumdfs(grid , i , j + 1) ;
-        sum += sumdfs(grid , i , j - 1) ;
-        sum += sumdfs(grid , i + 1 , j) ;
-        sum += sumdfs(grid , i - 1 , j) ;
+    int sumdfs(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0)
+            return 0;
 
-        return sum + tmp ; 
-        
+        int tmp = grid[i][j];
+        grid[i][j] = 0; // mark visited
+        long sum = tmp;
+
+        // explore all 4 directions
+        for (int d = 0; d < 4; ++d) {
+            int ni = i + dr[d];
+            int nj = j + dc[d];
+            sum += sumdfs(grid, ni, nj);
+        }
+
+        return sum;
     }
 
     int countIslands(vector<vector<int>>& grid, int k) {
+        n = grid.size();
+        m = grid[0].size();
+        int cnt = 0;
 
-         n = grid.size() ;
-         m = grid[0].size() ;
-        long sum = 0 , cnt = 0 ;
-
-        for(int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j < m ; j++){
-                if(grid[i][j] != 0) {
-                    sum += sumdfs(grid , i , j) ;
-                    if(sum % k == 0) cnt ++ ;
-                    sum = 0 ;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] != 0) {
+                    int islandSum = sumdfs(grid, i, j);
+                    if (islandSum % k == 0)
+                        cnt++;
                 }
             }
         }
 
-        return cnt ;
-        
+        return cnt;
     }
 };

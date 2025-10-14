a6ba55df7600d2836934de74aa281013057dda83
Kabeer Uppal <80846457+cosmoLegend@users.noreply.github.com>
2025-10-14 09:46:10 +0530

    Time: 21 ms (18.06%), Space: 15.5 MB (24.06%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+
+        int n = grid.size() ;
+
+        vector<vector<bool>>vis(n , vector<bool> (n , false)) ;
+
+        priority_queue<vector<int> , vector<vector<int>> , greater<>>pq ;
+        int maxTime = 0 ; 
+
+        pq.push({grid[0][0] , 0  , 0}) ;
+        vis[0][0] = 1 ;
+
+        while (!pq.empty()){
+            int maxDepth = pq.top()[0] ;
+            int row = pq.top()[1] ;
+            int col = pq.top()[2] ; 
+
+            if(row == n - 1 && col == n - 1) return maxDepth ; 
+
+            pq.pop() ;
+            vector<int> delrc = {1 , 0 , -1 , 0 , 1} ;
+
+
+            for (int i = 0 ; i < delrc.size() - 1 ; i++){
+                int nr = row + delrc[i] ;
+                int nc = col + delrc[i + 1] ;
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < n && vis[nr][nc] == 0){
+                    pq.push({max(grid[nr][nc] , maxDepth) , nr , nc}) ;
+                    vis[nr][nc] = 1 ;
+                }
+            }
+
+            maxTime = max (maxDepth , maxTime) ; 
+
+        }
+
+
+        return maxTime ; 
+        
+    }
+};
